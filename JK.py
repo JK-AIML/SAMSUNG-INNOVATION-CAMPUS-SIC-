b63eda71c4626ae2953834aabf3e728d5fde306f
JK_SINGH_666 <141818392+JK-AIML@users.noreply.github.com>
2025-02-17 09:48:42 +0000

    Error Handling

---
+++ JK.py
@@ -0,0 +1,8 @@
+# error handling 
+# type 1 error caused due to not declared variable 
+n=int(input("enter the number"))
+print("string"+num)
+
+#type 2 error cause due to different data types 
+n=int(input("enter the number"))
+print("string"+n)
